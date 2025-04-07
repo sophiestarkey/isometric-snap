4e7d941bfc47178b0aa4993f9966844968c14ef5
Sophie Starkey <136779521+sophiestarkey@users.noreply.github.com>
2025-04-08 00:25:13 +1200

    Refactor main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,83 +5,29 @@ interface Point {
 
 const canvas = document.getElementById("viewport") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-
+const cursor: Point = { x: 0, y: 0 };
 let side_length = 0;
 let height = 0;
-let cursor: Point = { x: 0, y: 0};
 
-document.body.oninput = (ev) => {
-	side_length = (document.getElementById("side_length_input") as HTMLInputElement).valueAsNumber;
-	height = side_length * Math.sqrt(3) / 2;
-};
-
-window.onmousemove = (ev) => {
-	cursor.x = Math.round(ev.clientX - canvas.getBoundingClientRect().left);
-	cursor.y = Math.round(ev.clientY - canvas.getBoundingClientRect().top);
-};
-
-window.onload = main;
-
-function main(): void
-{
-	document.body.dispatchEvent(new InputEvent("input"));
-	requestAnimationFrame(draw);
-}
-
-// returns the squared distance between two points
 function sqr_dst(a: Point, b: Point): number
 {
 	return (b.x - a.x) * (b.x - a.x) + (b.y - a.y) * (b.y - a.y);
 }
 
-function isometry_nearest_even(x: number, y: number): Point
+function isometry_nearest_even(point: Point): Point
 {
-	let result_x = Math.round(x / side_length) * side_length;
-	let result_y = Math.round(y / (2 * height)) * 2 * height;
-
-	return { x: result_x, y: result_y };
+	return {
+		x: Math.round(point.x / side_length) * side_length,
+		y: Math.round(point.y / (2 * height)) * 2 * height
+	};
 }
 
-function isometry_nearest_odd(x: number, y: number): Point
+function isometry_nearest_odd(point: Point): Point
 {
-	let result_x = (Math.round(x / side_length - 0.5) + 0.5) * side_length;
-	let result_y = (Math.round(y / (2 * height) - 0.5) + 0.5) * 2 * height;
-
-	return { x: result_x, y: result_y };
-}
-
-function draw(): void
-{
-	let nearest_even = isometry_nearest_even(cursor.x, cursor.y);
-	let nearest_odd = isometry_nearest_odd(cursor.x, cursor.y);
-
-	let closest = sqr_dst(nearest_even, cursor) < sqr_dst(nearest_odd, cursor) ? nearest_even : nearest_odd;
-
-	// background
-	ctx.fillStyle = "rgb(192, 192, 192)";
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-	// grid
-	ctx.strokeStyle = "rgb(160, 160, 160)";
-	draw_cartesian_grid();
-
-	// points and hexagons
-	ctx.fillStyle = "black";
-	ctx.strokeStyle = "rgb(64, 64, 64)";
-
-	draw_point(nearest_even.x, nearest_even.y);
-	draw_point(nearest_odd.x, nearest_odd.y);
-
-	draw_hexagon(nearest_even.x, nearest_even.y, Math.tan(Math.PI / 6) * side_length);
-	draw_hexagon(nearest_odd.x, nearest_odd.y, Math.tan(Math.PI / 6) * side_length);
-
-	ctx.fillStyle = "red";
-	ctx.strokeStyle = "red";
-	
-	draw_point(closest.x, closest.y);
-	draw_hexagon(closest.x, closest.y, Math.tan(Math.PI / 6) * side_length);
-
-	requestAnimationFrame(draw);
+	return {
+		x: (Math.round(point.x / side_length - 0.5) + 0.5) * side_length,
+		y: (Math.round(point.y / (2 * height) - 0.5) + 0.5) * 2 * height
+	};
 }
 
 function draw_cartesian_grid()
@@ -101,22 +47,71 @@ function draw_cartesian_grid()
 	}
 }
 
-function draw_hexagon(x: number, y: number, side_length: number)
+function draw_hexagon(point: Point, side_length: number)
 {
 	const height = side_length * Math.sqrt(3) / 2;
 	ctx.beginPath();
-	ctx.moveTo(x, y - side_length);
-	ctx.lineTo(x + height, y - side_length / 2);
-	ctx.lineTo(x + height, y + side_length / 2);
-	ctx.lineTo(x, y + side_length);
-	ctx.lineTo(x - height, y + side_length / 2);
-	ctx.lineTo(x - height, y - side_length / 2);
+	ctx.moveTo(point.x, point.y - side_length);
+	ctx.lineTo(point.x + height, point.y - side_length / 2);
+	ctx.lineTo(point.x + height, point.y + side_length / 2);
+	ctx.lineTo(point.x, point.y + side_length);
+	ctx.lineTo(point.x - height, point.y + side_length / 2);
+	ctx.lineTo(point.x - height, point.y - side_length / 2);
 	ctx.closePath();
 	ctx.stroke();
 }
 
-function draw_point(x: number, y: number)
+function draw_point(point: Point)
 {
 	const size = 4;
-	ctx.fillRect(x - size / 2, y - size / 2, size, size);
+	ctx.fillRect(point.x - size / 2, point.y - size / 2, size, size);
+}
+
+function draw(): void
+{
+	const nearest_even = isometry_nearest_even(cursor);
+	const nearest_odd = isometry_nearest_odd(cursor);
+	const closest = sqr_dst(nearest_even, cursor) < sqr_dst(nearest_odd, cursor) ? nearest_even : nearest_odd;
+
+	// background
+	ctx.fillStyle = "rgb(192, 192, 192)";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+	// grid
+	ctx.strokeStyle = "rgb(160, 160, 160)";
+	draw_cartesian_grid();
+
+	// points and hexagons
+	ctx.fillStyle = "black";
+	ctx.strokeStyle = "rgb(64, 64, 64)";
+
+	draw_point(nearest_even);
+	draw_point(nearest_odd);
+
+	draw_hexagon(nearest_even, Math.tan(Math.PI / 6) * side_length);
+	draw_hexagon(nearest_odd, Math.tan(Math.PI / 6) * side_length);
+
+	ctx.fillStyle = "red";
+	ctx.strokeStyle = "red";
+	
+	draw_point(closest);
+	draw_hexagon(closest, Math.tan(Math.PI / 6) * side_length);
+
+	requestAnimationFrame(draw);
+}
+
+function main(): void
+{
+	window.addEventListener("mousemove", (ev) => {
+		cursor.x = Math.round(ev.clientX - canvas.getBoundingClientRect().left);
+		cursor.y = Math.round(ev.clientY - canvas.getBoundingClientRect().top);
+	});
+
+	document.body.addEventListener("input", (ev) => {
+		side_length = (document.getElementById("side_length_input") as HTMLInputElement).valueAsNumber;
+		height = side_length * Math.sqrt(3) / 2;
+	});
+
+	document.body.dispatchEvent(new InputEvent("input"));
+	requestAnimationFrame(draw);
 }
